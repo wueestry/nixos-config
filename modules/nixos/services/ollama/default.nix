8dfc74e21e3f5a9bef7ccd681b24ba274f60f0f8
Ryan Wüest <ryan.wueest@protonmail.com>
2025-03-24 19:03:20 +0100

    Add ollama unstable

--- modules/nixos/services/ollama/default.nix
+++ modules/nixos/services/ollama/default.nix
@@ -4,23 +4,44 @@
   lib,
   pkgs,
   namespace,
+  inputs,
   ...
 }:
 with lib;
 with lib.${namespace};
 let
   cfg = config.${namespace}.services.ollama;
-  alpaca-cuda = pkgs.alpaca.override {
-    ollama = pkgs.ollama-cuda;
+  pkgs_unstable = import inputs.unstable {
+    system = "x86_64-linux";
+    config.allowUnfree = true;
   };
 in
 {
   options.${namespace}.services.ollama = with types; {
     enable = mkBoolOpt false "Enable ollama";
   };
+
+  disabledModules = [
+    "services/misc/ollama.nix"
+  ];
+  imports = [
+    "${inputs.unstable}/nixos/modules/services/misc/ollama.nix"
+  ];
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      alpaca-cuda
-    ];
+    services = {
+      ollama = {
+        enable = true;
+        port = 11434;
+        openFirewall = true;
+        acceleration = "cuda";
+        package = pkgs_unstable.ollama;
+      };
+      nextjs-ollama-llm-ui = {
+        enable = true;
+        ollamaUrl = "http://127.0.0.1:11434";
+        hostname = "localhost";
+        port = 3000;
+      };
+    };
   };
 }
